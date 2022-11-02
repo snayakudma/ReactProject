6ce0ef974d05470c6698a88454f54e1d3dcc1cfc
snayakudma <79699530+snayakudma@users.noreply.github.com>
2022-11-02 22:04:28 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul></ul>
+    <li>Bacon</li>
+    <li>Ramen</li>
+    <li>Noodles</li>
+  </div>,
+  document.getElementById("root")
+);
